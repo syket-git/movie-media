a3dae03f782a55c93819f11a27101e9183e6a01c
syketbb <syket@algosolver.com>
2023-01-09 15:58:31 +0600

    not found image added

--- components/pages/Home/sections/Body.tsx
+++ components/pages/Home/sections/Body.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { GlobalContext } from 'pages/_app';
 import React, { useContext } from 'react';
 import MovieCard from './MovieCard';
@@ -7,12 +8,21 @@ const Body = () => {
   console.log({ movies });
   return (
     <div className="container my-14">
-      <div className="grid grid-cols-6 gap-6">
-        {movies?.length > 0 &&
-          movies?.map((movie: any, i: number) => (
+      {movies?.length > 0 ? (
+        <div className="grid grid-cols-6 gap-6">
+          {movies?.map((movie: any, i: number) => (
             <MovieCard key={i} data={movie} />
           ))}
-      </div>
+        </div>
+      ) : (
+        <div className="w-full mt-20">
+          <img
+            src="./not-found.svg"
+            className="mx-auto w-[300px]"
+            alt="Not Found Image"
+          />
+        </div>
+      )}
     </div>
   );
 };
